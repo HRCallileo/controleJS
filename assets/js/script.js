2c9c49af6078b9f0509f8b7ddf15ae9e58e69f6d
leo le leu <leohrc76@gmail.com>
2023-01-20 14:47:43 +0100

    modif

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,12 @@
-let playlist = [
-    [
-        'Numb/Encore',
-        'Black Album',
-        'Jay-Z',
-        '2004',
-        'black-album.jpg'
-    ],
-    [
-        'All The Way UP',
-        'Plata O Plomo',
-        'Fat Joe',
-        '2016',
-        'plata-o-promo.jpg'
-    ],
-    [
-        'Me & My Girlsfriend',
-        'The Don Killuminati: The 7 Day Theory',
-        '2 Pac',
-        '1996',
-        '7-day-theory.jpg'
-    ],
-    [
-        'Runnin (Dying To Live)',
-        'Resurrection',
-        '2 Pac',
-        '2003',
-        'resurrection.jpg'
-    ],
-    [
-        'Break Ya Neck',
-        'Genesis',
-        'Busta Rhymes',
-        '2001',
-        'genesis.jpg'
-    ],
-    [
-        'Easy Skanking',
-        'Kaya',
-        'Bob Marley',
-        '1978',
-        'kaya.jpg'
-    ],
-    [
-        'A Milli',
-        'The carter IV',
-        'Lil Wayne',
-        '2011',
-        'the-carter-4.jpg'
-    ]
-];
+fetch(() => new Response(JSON.stringify(playlist)))
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(rowData => {
+      let row = document.createElement('tr');
+      rowData.forEach(cellData => {
+        let cell = document.createElement('td');
+        cell.textContent = cellData;
+        row.appendChild(cell);
+      });
+    document.thead.appendChild(row);
+  })});
