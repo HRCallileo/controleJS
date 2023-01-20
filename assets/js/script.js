9cdd4dc76635917489e934ca9ec53de1191bfc1a
leo le leu <leohrc76@gmail.com>
2023-01-20 14:11:41 +0100

    optimisation files

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+let playlist = [
+    [
+        'Numb/Encore',
+        'Black Album',
+        'Jay-Z',
+        '2004',
+        '1.jpg'
+    ],
+    [
+        'All The Way UP',
+        'Plata O Plomo',
+        'Fat Joe',
+        '2016',
+        '2.jpg'
+    ],
+    [
+        'Me & My Girlsfriend',
+        ' The Don Killuminati: The 7 Day Theory',
+        '2 Pac',
+        '1996',
+        '3.jpg'
+    ],
+    [
+        'Runnin (Dying To Live)',
+        'Resurrection ',
+        '2 Pac',
+        '2003',
+        '4.jpg'
+    ],
+    [
+        'Break Ya Neck',
+        'Genesis',
+        'Busta Rhymes',
+        '2001',
+        '5.jpg'
+    ],
+    [
+        'Easy Skanking',
+        'Kaya',
+        'Bob Marley',
+        '1978',
+        '6.jpg'
+    ],
+    [
+        'A Milli',
+        'The carter IV',
+        'Lil Wayne',
+        '2011',
+        '7.jpg'
+    ]
+];
