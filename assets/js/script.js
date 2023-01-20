d0ece98a716ce84fae07fce782d9b00217198e41
leo le leu <leohrc76@gmail.com>
2023-01-20 14:57:26 +0100

    mod

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
-fetch(() => new Response(JSON.stringify(playlist)))
-  .then(response => response.json())
-  .then(data => {
-    data.forEach(rowData => {
-      let row = document.createElement('tr');
-      rowData.forEach(cellData => {
-        let cell = document.createElement('td');
-        cell.textContent = cellData;
-        row.appendChild(cell);
-      });
-    document.thead.appendChild(row);
-  })});
+function getSong() {
+    let container = document.createElement("tr");
+    container.innerHTML = `
+    <th>${title}</th> 
+    <th>${album}</th> 
+    <th>${artist}</th> 
+    <th>${year}</th> 
+    <th>${poster}</th> 
+    <th>Retirer</th>
+    `;
+
+return container;
+}
+
+
+fetch(new Request("song.json")) 
+    .then((playlist) => (playlist.json()))
+    .then((playlist) => {
+    const song = playlist.results
+    let tableBody = document.getElementById('tableBody');
+    for(i of song) {
+        tableBody.append(getSong(i))
+    }
+});
